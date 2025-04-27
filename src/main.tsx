4018210febc9a1ae132afabd2e737bb4c15fd7b7
y01and3 <1791280644@qq.com>
2025-04-27 18:46:43 +0800

    feat: implement routing with React Router for main and edit views

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 import App from "./App.tsx";
+import EditApp from "./EditApp.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route element={<App />} path="/" />
+        <Route element={<EditApp />} path="/edit" />
+      </Routes>
     </BrowserRouter>
   </StrictMode>,
 );
