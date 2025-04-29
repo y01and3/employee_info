35d6edf399257424ee2153bd0f515974085ac900
y01and3 <1791280644@qq.com>
2025-04-29 19:52:48 +0800

    feat: refactor ProfileContext to use a defaultProfile for improved structure

--- src/hooks/profileContext.ts
+++ src/hooks/profileContext.ts
@@ -2,58 +2,65 @@ import type { Profile } from "../profile.type";
 
 import React from "react";
 
+const defaultProfile: Profile = {
+  name: {
+    top: 100,
+    left: 40,
+    context: "John Doe",
+  },
+  tag: {
+    top: 200,
+    left: 40,
+    context: [
+      { icon: { mod: "emoji", src: "👨‍💻" }, text: "Developer" },
+      { icon: { mod: "emoji", src: "🌍" }, text: "Traveler" },
+    ],
+  },
+  introduction: {
+    top: 300,
+    left: 40,
+    context: "Hello, I am John Doe, a software developer.",
+  },
+  social: {
+    top: 500,
+    left: 40,
+    context: [
+      {
+        icon: { mod: "emoji", src: "🐦" },
+        link: "https://twitter.com/johndoe",
+      },
+    ],
+  },
+  post: {
+    top: 700,
+    left: 40,
+    context: [
+      {
+        title: "My first post",
+        description: "This is my first post.",
+        date: new Date(),
+        link: "https://example.com/first-post",
+      },
+    ],
+  },
+  resume: {
+    top: 1500,
+    left: 50,
+    context: [
+      {
+        start: new Date(),
+        end: new Date(),
+        title: "Software Engineer at XYZ",
+      },
+    ],
+  },
+};
+
 const ProfileContext = React.createContext<{
   profile: Profile;
   setProfile: (profile: Profile) => void;
 }>({
-  profile: {
-    name: {
-      top: 0,
-      left: 0,
-      context: "John Doe",
-    },
-    tag: {
-      top: 0,
-      left: 0,
-      context: [
-        { icon: "👨‍💻", text: "Developer" },
-        { icon: "🌍", text: "Traveler" },
-      ],
-    },
-    introduction: {
-      top: 0,
-      left: 0,
-      context: "Hello, I am John Doe, a software developer.",
-    },
-    social: {
-      top: 0,
-      left: 0,
-      context: [{ icon: "🐦", link: "https://twitter.com/johndoe" }],
-    },
-    post: {
-      top: 0,
-      left: 0,
-      context: [
-        {
-          title: "My first post",
-          description: "This is my first post.",
-          date: new Date(),
-          link: "https://example.com/first-post",
-        },
-      ],
-    },
-    resume: {
-      top: 0,
-      left: 0,
-      context: [
-        {
-          start: new Date(),
-          end: new Date(),
-          title: "Software Engineer at XYZ",
-        },
-      ],
-    },
-  },
+  profile: defaultProfile,
   setProfile: (_: Profile) => {},
 });
 
@@ -63,4 +70,4 @@ const useProfile = () => {
   return React.useContext(ProfileContext);
 };
 
-export { ProfileContext, ProfileProvider, useProfile };
+export { ProfileContext, ProfileProvider, useProfile, defaultProfile };
