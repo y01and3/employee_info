eb60ec8efa8e87b88e45a60d156a46406db8460b
y01and3 <91734856+y01and3@users.noreply.github.com>
2025-05-06 10:16:51 +0800

    fix: change to hash router for github page

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { HeroUIProvider, ToastProvider } from "@heroui/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { HashRouter, Route, Routes } from "react-router";
 
 import App from "./App.tsx";
 import EditApp from "./EditApp.tsx";
@@ -11,12 +11,12 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HeroUIProvider>
       <ToastProvider />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route element={<App />} path="/employee_info/" />
           <Route element={<EditApp />} path="/employee_info/edit" />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </HeroUIProvider>
   </StrictMode>,
 );
